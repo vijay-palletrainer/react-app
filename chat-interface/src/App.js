e1a84c4d8827107b347935de28e9d93e043ef22a
vijay-palletrainer <palletrainer12@techpalle.com>
2025-02-05 19:15:50 +0530

    Update App.js

--- chat-interface/src/App.js
+++ chat-interface/src/App.js
@@ -15,7 +15,7 @@ const ChatInterface = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://bv28c3xsu3.execute-api.ap-south-1.amazonaws.com/prod/chat-bot", {
+      const response = await fetch("https://ykvv4099qe.execute-api.ap-south-1.amazonaws.com/prod/chat-bot", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
